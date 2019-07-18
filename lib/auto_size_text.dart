f32096716cd38a717ea91f773514e2648d6e8090
MichealReed <micheal.reed@outlook.com>
2019-07-17 20:56:51 -0500

    Update auto_size_text.dart

--- lib/auto_size_text.dart
+++ lib/auto_size_text.dart
@@ -3,7 +3,7 @@ library auto_size_text;
 
 import 'dart:async';
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter_web/widgets.dart';
 
 part 'src/auto_size_text.dart';
 part 'src/auto_size_group.dart';
